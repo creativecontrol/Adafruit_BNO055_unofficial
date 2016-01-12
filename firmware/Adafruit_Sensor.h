253c35339026a3bbcbf2bbd9b0b27e2c0537940f
Thadeus Reed <t@creativecontrol.cc>
2016-01-12 03:55:10 -0800

    added application.h to Adafruit_Sensor.h

--- firmware/Adafruit_Sensor.h
+++ firmware/Adafruit_Sensor.h
@@ -20,12 +20,7 @@
 #ifndef _ADAFRUIT_SENSOR_H
 #define _ADAFRUIT_SENSOR_H
 
-//#if ARDUINO >= 100
-// #include "Arduino.h"
-// #include "Print.h"
-//#else
-// #include "WProgram.h"
-//#endif
+#include "application.h"
 
 /* Intentionally modeled after sensors.h in the Android API:
  * https://github.com/android/platform_hardware_libhardware/blob/master/include/hardware/sensors.h */
